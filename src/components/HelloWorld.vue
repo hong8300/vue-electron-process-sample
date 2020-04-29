8a699f99e00fe0f4ec9609a84b2de1084db8de15
hong <hong@hongMBP2017.local>
2020-04-29 16:05:15 +0900

    first version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,111 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Vue + Eletron - ChildProcess 
+        </h1>
+         <v-btn v-if="!is_run" color="normal" dark v-on:click="clear_data">画面クリア</v-btn><br/><br/>
+
+        <!--        <H3>is_run {{ is_run }} </h3> -->
+         <v-btn v-if="!is_run" color="warning" dark v-on:click="exec('python','--version')">実行: python --version</v-btn> 本番ではフルパスで指定しないと動きません。<br/><br/>
+         <v-btn v-if="!is_run" color="warning" dark v-on:click="exec('ls','-l')">実行: ls -l</v-btn><br/><br/>
+         <v-btn v-if="!is_run" color="warning" dark v-on:click="exec('pwd','')">実行: pwd</v-btn> 本番時とデバック時ではカレントディレクトリが違います。<br/><br/>
+         <v-btn v-if="!is_run" color="warning" dark v-on:click="exec('/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code','')">実行: VSCode 起動</v-btn>
+         
+        <pre align="left">[ {{ output_data }} ]</pre>
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    // コンポーネント名
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    // コンポーネントで使用するデータ
+    data: function ()  {
+      return { 
+        output_data: "ここに実行結果がはいります。",
+        is_run: false,
+      }
+    },
+
+    // コンポーネントで使用する関数
+    methods : {
+      //
+      clear_data : function() {
+        this.output_data = "";
+      },
+      // 子プロセスを呼び出した結果を入れる
+      exec_data : function(data) {
+        console.log("function exec_data");
+        console.log(data)
+        this.output_data = data;
+      },
+
+      exec_error : function(data) {
+        console.log("function exec_error");
+        this.output_data = data;
+        // 実行中フラグOFF
+        this.is_run = false;
+      },
+
+      exec_exit : function(code) {
+        if(code == 0){
+          // 正常終了
+          console.log("function exec: normal end");
+        } else {
+          // 異常終了（未実装）
+          console.log("function exec: ERROR ");
+        }
+        // 実行中フラグOFF
+        this.is_run = false;
+      },
+
+      // コプロ実行関数
+      exec : function(cmd,param) {
+        console.log("IN function exec")
+        console.log(cmd,param)
+        // 外部プロセス実行
+        let childProcess = require("child_process");
+
+        // ここに外部コマンドを記述
+//        let p = childProcess.spawn('ls',['-lR']);
+//        let p = childProcess.spawn('python',['--version']);
+        let p;
+        if ( param == "" ) {
+          p = childProcess.spawn(cmd);
+        } else {
+          p = childProcess.spawn(cmd,[param]);
+        }
+        // プロセス実行中フラグ
+        this.is_run = true;
+
+        // UTF-8設定
+        p.stdout.setEncoding("utf-8");
+
+        // 出力を受け取る関数を踏力
+        p.stdout.on("data", this.exec_data);
+
+        // プロセスの実行失敗
+        p.on("error", this.exec_error);
+
+        // プロセス終了時のコールバック
+        p.on("exit", this.exec_exit);
+
+      }
+    }
+  }
+</script>
